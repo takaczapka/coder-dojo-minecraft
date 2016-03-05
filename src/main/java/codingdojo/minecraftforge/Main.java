0734d9de75f6c599b3a9ecee090347aef03d6f5f
Maciej Falski <takaczapka@gmail.com>
2016-03-05 12:08:12 +0000

    code formatting

--- src/main/java/codingdojo/minecraftforge/Main.java
+++ src/main/java/codingdojo/minecraftforge/Main.java
@@ -8,21 +8,21 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
 @Mod(modid = Main.MODID, version = Main.VERSION)
 public class Main {
-	public static final String MODID = "CodingDojoMod";
-	public static final String VERSION = "1.0";
-	
-	@EventHandler
-	public void init(FMLInitializationEvent whateverIWantHere) {
-		MinecraftForge.EVENT_BUS.register(new BlockBreakMessage());
+    public static final String MODID = "CodingDojoMod";
+    public static final String VERSION = "1.0";
+
+    @EventHandler
+    public void init(FMLInitializationEvent whateverIWantHere) {
+        MinecraftForge.EVENT_BUS.register(new BlockBreakMessage());
 //		MinecraftForge.EVENT_BUS.register(new ExplodingMinecarts());
-		MinecraftForge.EVENT_BUS.register(new SuperJump());
-		MinecraftForge.EVENT_BUS.register(new Parachute());
-		MinecraftForge.EVENT_BUS.register(new BlockFillerPositionSelector());
-	}
-	
-	  @EventHandler
-	  public void serverLoad(FMLServerStartingEvent event) {
-		  event.registerServerCommand(new FlamingPigs());
-		  event.registerServerCommand(new BlockFillCommand());
-	  }
+        MinecraftForge.EVENT_BUS.register(new SuperJump());
+        MinecraftForge.EVENT_BUS.register(new Parachute());
+        MinecraftForge.EVENT_BUS.register(new BlockFillerPositionSelector());
+    }
+
+    @EventHandler
+    public void serverLoad(FMLServerStartingEvent event) {
+        event.registerServerCommand(new FlamingPigs());
+        event.registerServerCommand(new BlockFillCommand());
+    }
 }
